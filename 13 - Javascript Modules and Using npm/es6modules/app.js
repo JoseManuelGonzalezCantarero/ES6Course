b3e483114650adcbc35e6a4d9630e1e4bc6b2731
Manu <jgonzalezcantarero@gmail.com>
2019-03-01 10:11:35 +0100

    Creating you own modules

--- 13 - Javascript Modules and Using npm/es6modules/app.js
+++ 13 - Javascript Modules and Using npm/es6modules/app.js
@@ -1,6 +1,20 @@
 import { uniq } from 'lodash';
+
 import insane from 'insane';
+
 import jsonp from 'jsonp';
+
+import {
+  apiKey as key,
+  url,
+  sayHi,
+  old,
+  dog,
+} from './src/config';
+
+console.log(key, url);
+
+sayHi('manu');
 
 const ages = [1, 1, 4, 52, 12, 4];
 
